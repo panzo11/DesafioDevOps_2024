cccf562d686bc6f23e2ce6660fb0f6758d5864b2
panzo11 <panzopedro26@gmail.com>
2024-10-21 15:52:14 +0100

    kubernetes dashboard conf 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World-6 4 agora vai!!!');
+  res.send('Hello a World-6 4 agora vai!!!');
 });
 
 app.listen(port, () => {
